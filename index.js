bccdbbe27d0b9428089dfa23e83072e5aa9f8174
Tiago Marques <thymarques@gmail.com>
2021-07-14 19:04:28 -1000

    parsing HTTP Methods

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ var url = require('url');
 var server = http.createServer((req,res) => {
 
     var parsedUrl = url.parse(req.url, true);
-
     var path = parsedUrl.pathname;
-
     var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+    var method = req.method.toLowerCase();
 
     res.end('hello world');
 
-    console.log('request received on: ' + trimmedPath);
+    console.log('request received on: ' + trimmedPath + ' \nThis method: ' + method);
 });
 
 server.listen(3000,()=>{
