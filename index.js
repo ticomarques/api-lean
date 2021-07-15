6285666af9ed18f68614f757a25bf93a6385007c
Tiago Marques <thymarques@gmail.com>
2021-07-14 19:01:34 -1000

    parsing request paths

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var http = require('http');
+var url = require('url');
 
 
 var server = http.createServer((req,res) => {
+
+    var parsedUrl = url.parse(req.url, true);
+
+    var path = parsedUrl.pathname;
+
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
     res.end('hello world');
+
+    console.log('request received on: ' + trimmedPath);
 });
 
 server.listen(3000,()=>{
