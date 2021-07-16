789bd74b42e7ca3f065c8a04d7d7f558ef2f913e
Tiago Marques <thymarques@gmail.com>
2021-07-15 15:04:19 -1000

    parsing payloads and readme

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 
 var server = http.createServer((req,res) => {
@@ -10,10 +11,18 @@ var server = http.createServer((req,res) => {
     var method = req.method.toLowerCase();
     var queryStringObject = parsedUrl.query;
     var headers = req.headers;
+    var decoder = new StringDecoder('utf-8');
+    
+    var buffer = '';
+    req.on('data',(data) => {
+        buffer += decoder.write(data);
+    });
+    req.on('end',()=>{
+        buffer += decoder.end();
 
-
-    res.end('hello world');
-    console.log('Headers: ', headers);
+        res.end('hello world');
+        console.log('Payload: ', buffer);
+    });   
 });
 
 server.listen(3000,()=>{
